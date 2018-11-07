49fa729b19d18ee023130721fab4ed3851ea7514
AnShuai666 <1028792866@qq.com>
2018-11-07 10:51:30 +0800

    Add some files and codes!

--- src/qcit_util/ini_parser.h
+++ src/qcit_util/ini_parser.h
@@ -5,4 +5,26 @@
 #ifndef RECONSTRUCTION_INI_PARSER_H
 #define RECONSTRUCTION_INI_PARSER_H
 
+#include <istream>
+#include <string>
+#include <map>
+
+#include "qcit_util/defines.h"
+
+UTIL_NAMESPACE_BEGIN
+
+/** Parses a file in INI format and places key/value pairs in the map. */
+void
+parse_ini (std::istream& stream, std::map<std::string, std::string>* map);
+
+/**
+ * Writes an INI file for the key/value pairs in the map.
+ * Section names are part of the key, separated with a dot from the key.
+ */
+void
+write_ini (std::map<std::string, std::string> const& map, std::ostream& stream);
+
+UTIL_NAMESPACE_END
+
+
 #endif //RECONSTRUCTION_INI_PARSER_H
